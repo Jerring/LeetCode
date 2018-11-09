f8fd9b1b87ecd715ffc4d9499a5a69f7064640e3
Jerring <3182730575@qq.com>
2018-11-09 09:49:10 +0800

    Add LeetCode_00141

---
+++ src/com/github/jerring/leetcode/LeetCode_00141.java
@@ -0,0 +1,17 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00141 {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
