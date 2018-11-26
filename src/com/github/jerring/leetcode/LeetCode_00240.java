851b9d287ac180e5107144c03b8a812b43ab52d7
Jerring <3182730575@qq.com>
2018-11-26 20:52:20 +0800

    Add LeetCode_00240

---
+++ src/com/github/jerring/leetcode/LeetCode_00240.java
@@ -0,0 +1,19 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00240 {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = matrix.length - 1;
+        int j = 0;
+        while (i >= 0 && j < matrix[0].length) {
+            if (matrix[i][j] < target) {
+                ++j;
+            } else if (matrix[i][j] > target) {
+                --i;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
