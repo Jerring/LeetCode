d78df8439bd16f9586da007da2bb078fcbaaaf78
Jerring <3182730575@qq.com>
2018-11-28 13:02:20 +0800

    Add LeetCode_00443

--- src/com/github/jerring/leetcode/LeetCode_00443.java
+++ src/com/github/jerring/leetcode/LeetCode_00443.java
@@ -3,15 +3,20 @@ package com.github.jerring.leetcode;
 public class LeetCode_00443 {
 
     public int compress(char[] chars) {
+        // n 为压缩后数组的新长度
         int n = 0;
+        // i 用来遍历原数组
         int i = 0;
         while (i < chars.length){
+            // 每次保存一个字符
             chars[n++] = chars[i++];
+            // 统计该字符的个数
             int cnt = 1;
             while (i < chars.length && chars[i] == chars[i - 1]) {
                 ++i;
                 ++cnt;
             }
+            // 如果该字符的个数大于 1，则加上字符个数（需要进行转换）；如果等于 1，则不用加
             if (cnt > 1) {
                 for (char c : Integer.toString(cnt).toCharArray()) {
                     chars[n++] = c;
