af0d90c9d9015cf8b39eccc054f71cb3560f56ee
Jerring <3182730575@qq.com>
2018-12-03 10:10:38 +0800

    Add LeetCode_00383

---
+++ src/com/github/jerring/leetcode/LeetCode_00383.java
@@ -0,0 +1,17 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00383 {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < magazine.length(); ++i) {
+            ++cnt[magazine.charAt(i) - 'a'];
+        }
+        for (int i = 0; i < ransomNote.length(); ++i) {
+            if (--cnt[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
