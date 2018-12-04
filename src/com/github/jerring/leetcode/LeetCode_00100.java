b8f206dc06f7bfef47a20e806847fe6c729413ea
Jerring <3182730575@qq.com>
2018-12-04 16:40:21 +0800

    Add LeetCode_00100

--- src/com/github/jerring/leetcode/LeetCode_00100.java
+++ src/com/github/jerring/leetcode/LeetCode_00100.java
@@ -2,6 +2,7 @@ package com.github.jerring.leetcode;
 
 public class LeetCode_00100 {
 
+    // 递归版本
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == q) {
             return true;
@@ -11,4 +12,35 @@ public class LeetCode_00100 {
         }
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+
+    // 迭代版本
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if (p == null && q == null) {
+//            return true;
+//        }
+//        if (p == null || q == null) {
+//            return false;
+//        }
+//        LinkedList<TreeNode> stack = new LinkedList<>();
+//        stack.push(p);
+//        stack.push(q);
+//        while (!stack.isEmpty()) {
+//            p = stack.pop();
+//            q = stack.pop();
+//            if (p == null && q == null) {
+//                continue;
+//            }
+//            if (p == null || q == null) {
+//                return false;
+//            }
+//            if (p.val != q.val) {
+//                return false;
+//            }
+//            stack.push(p.left);
+//            stack.push(q.left);
+//            stack.push(p.right);
+//            stack.push(q.right);
+//        }
+//        return true;
+//    }
 }
