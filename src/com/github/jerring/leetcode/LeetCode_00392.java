a780b262e3333a2a4517081fcff6d03c4fba44c8
Jerring <3182730575@qq.com>
2018-12-28 20:07:26 +0800

    Add LeetCode_00392

--- src/com/github/jerring/leetcode/LeetCode_00392.java
+++ src/com/github/jerring/leetcode/LeetCode_00392.java
@@ -14,4 +14,37 @@ public class LeetCode_00392 {
         }
         return true;
     }
+
+//    // 进阶挑战（有大量输入的 S，T 相同）
+//    public boolean isSubsequence(String s, String t) {
+//        // 数组的每个元素是一个列表，用来存储对应字符的下标集合
+//        List<Integer>[] map = new ArrayList[26];
+//        for (int i = 0; i < t.length(); ++i) {
+//            int tmp = t.charAt(i) - 'a';
+//            if (map[tmp] == null) {
+//                map[tmp] = new ArrayList<>();
+//            }
+//            map[tmp].add(i);
+//        }
+//        int index = 0;
+//        for (char c : s.toCharArray()) {
+//            List<Integer> list = map[c - 'a'];
+//            // 对应列表不存在
+//            if (list == null) {
+//                return false;
+//            }
+//            // 在对应列表中进行二分查找，得到在列表中的插入点
+//            int indexOfList = Collections.binarySearch(list, index);
+//            if (indexOfList < 0) {
+//                indexOfList = -indexOfList - 1;
+//            }
+//            // 没找到
+//            if (indexOfList == list.size()) {
+//                return false;
+//            }
+//            // 更新 index
+//            index = list.get(indexOfList) + 1;
+//        }
+//        return true;
+//    }
 }
