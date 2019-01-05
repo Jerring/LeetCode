0c9b583ee89df5a761906c97df538b21a85c9241
Jerring <3182730575@qq.com>
2019-01-05 17:33:38 +0800

    Add LeetCode_00744

---
+++ src/com/github/jerring/leetcode/LeetCode_00744.java
@@ -0,0 +1,18 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00744 {
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        int lo = 0;
+        int hi = letters.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target >= letters[mid]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo == letters.length ? letters[0] : letters[lo];
+    }
+}
