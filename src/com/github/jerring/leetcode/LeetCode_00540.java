8867a7520d4ba2f96748b7d859108dd97532628f
Jerring <3182730575@qq.com>
2019-01-06 23:48:00 +0800

    Add LeetCode_00540

--- src/com/github/jerring/leetcode/LeetCode_00540.java
+++ src/com/github/jerring/leetcode/LeetCode_00540.java
@@ -3,16 +3,19 @@ package com.github.jerring.leetcode;
 public class LeetCode_00540 {
 
     public int singleNonDuplicate(int[] nums) {
+        // 可能的上下限
         int lo = 0;
         int hi = nums.length - 1;
+        // 缩小范围直至只有一个元素
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
+            // 将奇数下标变为偶数下标
             if ((mid & 1) == 1) {
                 --mid;
             }
-            if (nums[mid] == nums[mid + 1]) {
+            if (nums[mid] == nums[mid + 1]) {   // 说明 mid 之前都是元素成对存在的，单个元素在后面
                 lo = mid + 2;
-            } else {
+            } else {                            // 说明单个元素下标不超过 mid
                 hi = mid;
             }
         }
