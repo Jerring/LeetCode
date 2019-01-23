6073de71a1c735b3bedd5087806bd6ff1b973587
Jerring <3182730575@qq.com>
2019-01-23 22:42:20 +0800

    Add LeetCode_00078

--- src/com/github/jerring/leetcode/LeetCode_00078.java
+++ src/com/github/jerring/leetcode/LeetCode_00078.java
@@ -5,18 +5,34 @@ import java.util.List;
 
 public class LeetCode_00078 {
 
+//    public List<List<Integer>> subsets(int[] nums) {
+//        int n = nums.length;
+//        int m = 1 << n;
+//        List<List<Integer>> res = new ArrayList<>(m);
+//        for (int i = 0; i < m; ++i) {
+//            List<Integer> list = new ArrayList<>();
+//            for (int j = 0; j < n; ++j) {
+//                if (((i >> j) & 1) == 1) {
+//                    list.add(nums[j]);
+//                }
+//            }
+//            res.add(list);
+//        }
+//        return res;
+//    }
+
     public List<List<Integer>> subsets(int[] nums) {
-        int n = nums.length;
-        int m = 1 << n;
-        List<List<Integer>> res = new ArrayList<>(m);
-        for (int i = 0; i < m; ++i) {
-            List<Integer> list = new ArrayList<>();
-            for (int j = 0; j < n; ++j) {
-                if (((i >> j) & 1) == 1) {
-                    list.add(nums[j]);
-                }
+        List<List<Integer>> res = new ArrayList<>(1 << nums.length);
+        res.add(new ArrayList<>());
+        for (int num : nums) {
+            // 枚举原来的每一个子集，把当前元素加入到所有子集中
+            int size = res.size();
+            for (int i = 0; i < size; ++i) {
+                // 拷贝一份
+                List<Integer> list = new ArrayList<>(res.get(i));
+                list.add(num);
+                res.add(list);
             }
-            res.add(list);
         }
         return res;
     }
