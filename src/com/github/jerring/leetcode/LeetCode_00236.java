d47afd6d51f72c25ef3355449f112f926c1181c4
Jerring <3182730575@qq.com>
2019-03-06 13:23:47 +0800

    Add LeetCode_00236

---
+++ src/com/github/jerring/leetcode/LeetCode_00236.java
@@ -0,0 +1,13 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
