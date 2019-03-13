170a1eba0779a8461a7d3ac0c91c3cb25c28fa7a
Jerring <3182730575@qq.com>
2019-03-13 17:32:52 +0800

    Add LeetCode_00876

---
+++ src/com/github/jerring/leetcode/LeetCode_00876.java
@@ -0,0 +1,14 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00876 {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
