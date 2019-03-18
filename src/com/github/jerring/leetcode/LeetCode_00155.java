c548d4fd45712ede8a0c991022b1f22a063aa99b
Jerring <3182730575@qq.com>
2019-03-18 15:04:08 +0800

    Add LeetCode_00155

--- src/com/github/jerring/leetcode/LeetCode_00155.java
+++ src/com/github/jerring/leetcode/LeetCode_00155.java
@@ -4,69 +4,71 @@ import java.util.Stack;
 
 public class LeetCode_00155 {
 
-//    class MinStack {
-//        private Stack<Integer> numStack;
-//        private Stack<Integer> minStack;
-//
-//        /** initialize your data structure here. */
-//        public MinStack() {
-//            numStack = new Stack<>();
-//            minStack = new Stack<>();
-//        }
-//
-//        public void push(int x) {
-//            numStack.push(x);
-//            if (minStack.isEmpty() || x <= minStack.peek()) {
-//                minStack.push(x);
-//            }
-//        }
-//
-//        public void pop() {
-//            int x = numStack.pop();
-//            if (x == minStack.peek()) {
-//                minStack.pop();
-//            }
-//        }
-//
-//        public int top() {
-//            return numStack.peek();
-//        }
-//
-//        public int getMin() {
-//            return minStack.peek();
-//        }
-//    }
-
+    // 运用两个栈
     class MinStack {
-        private Stack<Integer> stack;
-        private int min;
+        private Stack<Integer> numStack;
+        private Stack<Integer> minStack;
 
         /** initialize your data structure here. */
         public MinStack() {
-            stack = new Stack<>();
-            min = Integer.MAX_VALUE;
+            numStack = new Stack<>();
+            minStack = new Stack<>();
         }
 
         public void push(int x) {
-            if (x <= min) {
-                stack.push(min);
-                min = x;
+            numStack.push(x);
+            if (minStack.isEmpty() || x <= minStack.peek()) {
+                minStack.push(x);
             }
-            stack.push(x);
         }
 
         public void pop() {
-            if (min == stack.pop()) {
-                min = stack.pop();
+            int x = numStack.pop();
+            if (x == minStack.peek()) {
+                minStack.pop();
             }
         }
 
         public int top() {
-            return stack.peek();
+            return numStack.peek();
         }
 
         public int getMin() {
-            return min;
+            return minStack.peek();
         }
     }
+
+//    // 运用一个栈 + 一个变量
+//    class MinStack {
+//        private Stack<Integer> stack;
+//        private int min;
+//
+//        /** initialize your data structure here. */
+//        public MinStack() {
+//            stack = new Stack<>();
+//            min = Integer.MAX_VALUE;
+//        }
+//
+//        public void push(int x) {
+//            if (x <= min) {
+//                stack.push(min);
+//                min = x;
+//            }
+//            stack.push(x);
+//        }
+//
+//        public void pop() {
+//            if (min == stack.pop()) {
+//                min = stack.pop();
+//            }
+//        }
+//
+//        public int top() {
+//            return stack.peek();
+//        }
+//
+//        public int getMin() {
+//            return min;
+//        }
+//    }
 }
