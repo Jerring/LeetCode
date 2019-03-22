761d4127ed9177f29752b0144ac71a0be940f722
Jerring <3182730575@qq.com>
2019-03-22 20:32:31 +0800

    Add LeetCode_00191

--- src/com/github/jerring/leetcode/LeetCode_00191.java
+++ src/com/github/jerring/leetcode/LeetCode_00191.java
@@ -32,6 +32,15 @@ public class LeetCode_00191 {
 //    }
 
     public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n + (n >>> 4)) & 0x0f0f0f0f;
+        n = n + (n >>> 8);
+        n = n + (n >>> 16);
+        return n & 0x3f;
     }
+
+//    public int hammingWeight(int n) {
+//        return Integer.bitCount(n);
+//    }
 }
