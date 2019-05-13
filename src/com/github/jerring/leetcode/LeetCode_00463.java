f80345767eef91282b2fc4654f2e26099f3d7465
Jerring <3182730575@qq.com>
2019-05-13 12:46:55 +0800

    Add LeetCode_00463

--- src/com/github/jerring/leetcode/LeetCode_00463.java
+++ src/com/github/jerring/leetcode/LeetCode_00463.java
@@ -8,6 +8,7 @@ public class LeetCode_00463 {
 //        for (int i = 0; i < n; ++i) {
 //            for (int j = 0; j < m; ++j) {
 //                if (grid[i][j] == 1) {
+//                    // 直接加（边缘或相邻是水域）
 //                    if (i - 1 < 0 || grid[i - 1][j] == 0) ++res;
 //                    if (j - 1 < 0 || grid[i][j - 1] == 0) ++res;
 //                    if (i + 1 >= n || grid[i + 1][j] == 0) ++res;
@@ -24,6 +25,7 @@ public class LeetCode_00463 {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (grid[i][j] == 1) {
+                    // 先加后减（上边或左边是陆地）
                     res += 4;
                     if (i > 0 && grid[i - 1][j] == 1) res -= 2;
                     if (j > 0 && grid[i][j - 1] == 1) res -= 2;
