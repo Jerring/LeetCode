e6c33d033c119dd155d6e1713e737780be072b57
Jerring <3182730575@qq.com>
2019-05-25 20:44:53 +0800

    Add LeetCode_00322

--- src/com/github/jerring/leetcode/LeetCode_00322.java
+++ src/com/github/jerring/leetcode/LeetCode_00322.java
@@ -4,16 +4,29 @@ import java.util.Arrays;
 
 public class LeetCode_00322 {
 
+//    public int coinChange(int[] coins, int amount) {
+//        int[] dp = new int[amount + 1];
+//        // 初始化 dp
+//        Arrays.fill(dp, amount + 1);
+//        dp[0] = 0;
+//        for (int i = 1; i <= amount; ++i) {
+//            for (int coin : coins) {
+//                if (i >= coin) {
+//                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+//                }
+//            }
+//        }
+//        return dp[amount] != amount + 1 ? dp[amount] : -1;
+//    }
+
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        // 初始化 dp
+        // 恰好初始化 dp[0] = 0, 其余为无穷
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
-        for (int i = 1; i <= amount; ++i) {
-            for (int coin : coins) {
-                if (i >= coin) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
         return dp[amount] != amount + 1 ? dp[amount] : -1;
