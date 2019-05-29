d431d399f259b16847da94c3e587d7caf35f48b6
Jerring <3182730575@qq.com>
2019-05-29 19:24:57 +0800

    Add LeetCode_00095

--- src/com/github/jerring/leetcode/LeetCode_00095.java
+++ src/com/github/jerring/leetcode/LeetCode_00095.java
@@ -5,31 +5,64 @@ import java.util.List;
 
 public class LeetCode_00095 {
 
+//    public List<TreeNode> generateTrees(int n) {
+//        if (n < 1) {
+//            return new ArrayList<>();
+//        }
+//        return dfs(1, n);
+//    }
+//
+//    private List<TreeNode> dfs(int l, int r) {
+//        List<TreeNode> res = new ArrayList<>();
+//        if (l > r) {
+//            res.add(null);
+//            return res;
+//        }
+//        for (int i = l; i <= r; ++i) {
+//            List<TreeNode> leftList = dfs(l, i - 1);
+//            List<TreeNode> rightList = dfs(i + 1, r);
+//            for (TreeNode left : leftList) {
+//                for (TreeNode right : rightList) {
+//                    TreeNode root = new TreeNode(i);
+//                    root.left = left;
+//                    root.right = right;
+//                    res.add(root);
+//                }
+//            }
+//        }
+//        return res;
+//    }
+
     public List<TreeNode> generateTrees(int n) {
         if (n < 1) {
             return new ArrayList<>();
         }
-        return dfs(1, n);
-    }
-
-    private List<TreeNode> dfs(int l, int r) {
-        List<TreeNode> res = new ArrayList<>();
-        if (l > r) {
-            res.add(null);
-            return res;
-        }
-        for (int i = l; i <= r; ++i) {
-            List<TreeNode> leftList = dfs(l, i - 1);
-            List<TreeNode> rightList = dfs(i + 1, r);
-            for (TreeNode left : leftList) {
-                for (TreeNode right : rightList) {
-                    TreeNode root = new TreeNode(i);
-                    root.left = left;
-                    root.right = right;
-                    res.add(root);
+        List<TreeNode>[] dp = new List[n + 1];
+        dp[0] = new ArrayList<>();
+        dp[0].add(null);
+        for (int i = 1; i <= n; i++) {
+            dp[i] = new ArrayList<>();
+            for (int j = 1; j <= i; j++) {
+                for (TreeNode nodeL : dp[j - 1]) {
+                    for (TreeNode nodeR : dp[i - j]) {
+                        TreeNode node = new TreeNode(j);
+                        node.left = nodeL;              // 复用
+                        node.right = copy(nodeR, j);    // 加上偏移量复制一份
+                        dp[i].add(node);
+                    }
                 }
             }
         }
-        return res;
+        return dp[n];
+    }
+
+    private TreeNode copy(TreeNode root, int offset) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(root.val + offset);
+        node.left = copy(root.left, offset);
+        node.right = copy(root.right, offset);
+        return node;
     }
 }
