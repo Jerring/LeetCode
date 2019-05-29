6be6ceb92fa2ac538c18d1ddd23a1adfcb7ea1fe
Jerring <3182730575@qq.com>
2019-05-29 16:44:40 +0800

    Add LeetCode_00096

---
+++ src/com/github/jerring/leetcode/LeetCode_00096.java
@@ -0,0 +1,15 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00096 {
+
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
