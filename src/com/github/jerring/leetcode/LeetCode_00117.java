e666d405efe64a766823e4285129b8e3ef545306
Jerring <3182730575@qq.com>
2019-05-30 19:15:14 +0800

    Add LeetCode_00117

---
+++ src/com/github/jerring/leetcode/LeetCode_00117.java
@@ -0,0 +1,42 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00117 {
+
+    public Node connect(Node root) {
+        Node cur = root;
+        while (cur != null) {
+            Node dummy = new Node();
+            Node tail = dummy;
+            while (cur != null) {
+                if (cur.left != null) {
+                    tail.next = cur.left;
+                    tail = tail.next;
+                }
+                if (cur.right != null) {
+                    tail.next = cur.right;
+                    tail = tail.next;
+                }
+                cur = cur.next;
+            }
+            cur = dummy.next;
+        }
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
