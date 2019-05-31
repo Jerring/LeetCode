e08d4698ddf02cbde39a23c19ddf367746b48b25
Jerring <3182730575@qq.com>
2019-05-31 16:10:09 +0800

    Add LeetCode_00226

--- src/com/github/jerring/leetcode/LeetCode_00226.java
+++ src/com/github/jerring/leetcode/LeetCode_00226.java
@@ -1,38 +1,35 @@
 package com.github.jerring.leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class LeetCode_00226 {
 
-//    public TreeNode invertTree(TreeNode root) {
-//        if (root != null) {
-//            TreeNode left = root.left;
-//            TreeNode right = root.right;
-//            root.left = invertTree(right);
-//            root.right = invertTree(left);
-//        }
-//        return root;
-//    }
-
     public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            TreeNode tmp = node.left;
-            node.left = node.right;
-            node.right = tmp;
-            if (node.left != null) {
-                queue.offer(node.left);
-            }
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
+        if (root != null) {
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+            root.left = invertTree(right);
+            root.right = invertTree(left);
         }
         return root;
     }
+
+//    public TreeNode invertTree(TreeNode root) {
+//        if (root == null) {
+//            return null;
+//        }
+//        Queue<TreeNode> queue = new ArrayDeque<>();
+//        queue.offer(root);
+//        while (!queue.isEmpty()) {
+//            TreeNode node = queue.poll();
+//            TreeNode tmp = node.left;
+//            node.left = node.right;
+//            node.right = tmp;
+//            if (node.left != null) {
+//                queue.offer(node.left);
+//            }
+//            if (node.right != null) {
+//                queue.offer(node.right);
+//            }
+//        }
+//        return root;
+//    }
 }
