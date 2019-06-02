a1a55b6fd9ab93745b90bbfc3320a9931a8575e8
Jerring <3182730575@qq.com>
2019-06-02 19:00:41 +0800

    Add LeetCode_00543

---
+++ src/com/github/jerring/leetcode/LeetCode_00543.java
@@ -0,0 +1,20 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00543 {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] res = new int[1];
+        dfs(root, res);
+        return res[0];
+    }
+
+    private int dfs(TreeNode root, int[] res) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left, res);
+        int right = dfs(root.right, res);
+        res[0] = Math.max(res[0], left + right);
+        return 1 + Math.max(left, right);
+    }
+}
