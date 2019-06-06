773d6e3c37d559da5f8402bd767c8da5c100f910
Jerring <3182730575@qq.com>
2019-06-06 21:33:13 +0800

    Add LeetCode_00263

---
+++ src/com/github/jerring/leetcode/LeetCode_00263.java
@@ -0,0 +1,20 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00263 {
+
+    public boolean isUgly(int num) {
+        if (num < 1) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
