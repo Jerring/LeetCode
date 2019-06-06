9928e906d06359e6790b772570e72a5e49285817
Jerring <3182730575@qq.com>
2019-06-06 10:03:59 +0800

    Add LeetCode_00401

---
+++ src/com/github/jerring/leetcode/LeetCode_00401.java
@@ -0,0 +1,19 @@
+package com.github.jerring.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_00401 {
+
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 60; ++j) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == num) {
+                    res.add(String.format("%d:%02d", i, j));
+                }
+            }
+        }
+        return res;
+    }
+}
