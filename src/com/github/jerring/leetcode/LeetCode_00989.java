6357bba178eb18ca5cb2434df9058954c7f8f95b
Jerring <3182730575@qq.com>
2019-06-14 21:44:15 +0800

    Add LeetCode_00989

--- src/com/github/jerring/leetcode/LeetCode_00989.java
+++ src/com/github/jerring/leetcode/LeetCode_00989.java
@@ -7,20 +7,13 @@ import java.util.List;
 public class LeetCode_00989 {
 
     public List<Integer> addToArrayForm(int[] A, int K) {
-        for (int i = A.length - 1; i >= 0 && K != 0; --i) {
-            K += A[i];
-            A[i] = K % 10;
-            K /= 10;
-        }
         List<Integer> res = new ArrayList<>();
-        while (K != 0) {
+        for (int i = A.length - 1; i >= 0 || K != 0; --i) {
+            K += i >= 0 ? A[i] : 0;
             res.add(K % 10);
             K /= 10;
         }
         Collections.reverse(res);
-        for (int a : A) {
-            res.add(a);
-        }
         return res;
     }
 }
