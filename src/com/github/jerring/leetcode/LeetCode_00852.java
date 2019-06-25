23f7e312efd5b06adb55be96b10858bcb9815b9f
Jerring <3182730575@qq.com>
2019-06-25 10:57:25 +0800

    Add LeetCode_00852

---
+++ src/com/github/jerring/leetcode/LeetCode_00852.java
@@ -0,0 +1,14 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00852 {
+
+    public int peakIndexInMountainArray(int[] A) {
+        int l = 0, r = A.length - 1;
+        while (l < r) {
+            int mid = l + r >>> 1;
+            if (A[mid] > A[mid + 1]) r = mid;
+            else l = mid + 1;
+        }
+        return r;
+    }
+}
