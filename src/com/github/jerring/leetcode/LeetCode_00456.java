580b2e287629bfe4b4fb6bd52a2d71df5c27cea1
Jerring <3182730575@qq.com>
2019-06-26 20:06:20 +0800

    Add LeetCode_00456

--- src/com/github/jerring/leetcode/LeetCode_00456.java
+++ src/com/github/jerring/leetcode/LeetCode_00456.java
@@ -6,14 +6,14 @@ import java.util.Deque;
 public class LeetCode_00456 {
 
     public boolean find132pattern(int[] nums) {
-        int aj = Integer.MIN_VALUE;
+        int ak = Integer.MIN_VALUE;
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = nums.length - 1; i >= 0; --i) {
-            if (nums[i] < aj) {
+            if (nums[i] < ak) {
                 return true;
             }
             while (!stack.isEmpty() && nums[i] > stack.peek()) {
-                aj = stack.pop();
+                ak = stack.pop();
             }
             stack.push(nums[i]);
         }
