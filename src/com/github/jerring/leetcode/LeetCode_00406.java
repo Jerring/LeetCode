19562a07607ac845eae5d0a951aa151eed01ce07
Jerring <3182730575@qq.com>
2019-06-27 15:20:36 +0800

    Add LeetCode_00406

--- src/com/github/jerring/leetcode/LeetCode_00406.java
+++ src/com/github/jerring/leetcode/LeetCode_00406.java
@@ -1,21 +1,33 @@
 package com.github.jerring.leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LeetCode_00406 {
+
+//    public int[][] reconstructQueue(int[][] people) {
+//        // 按 h 降序、k 升序进行排序
+//        Arrays.sort(people, (o1, o2) -> o1[0] != o2[0] ? Integer.compare(o2[0], o1[0]) : Integer.compare(o1[1], o2[1]));
+//        for (int i = 0; i < people.length; ++i) {
+//            int[] p = people[i];
+//            // 后移一位，让出插入点
+//            for (int j = i; j > p[1]; --j) {
+//                people[j] = people[j - 1];
+//            }
+//            // 插入到合适位置
+//            people[p[1]] = p;
+//        }
+//        return people;
+//    }
 
     public int[][] reconstructQueue(int[][] people) {
         // 按 h 降序、k 升序进行排序
         Arrays.sort(people, (o1, o2) -> o1[0] != o2[0] ? Integer.compare(o2[0], o1[0]) : Integer.compare(o1[1], o2[1]));
-        for (int i = 0; i < people.length; ++i) {
-            int[] p = people[i];
-            // 后移一位，让出插入点
-            for (int j = i; j > p[1]; --j) {
-                people[j] = people[j - 1];
-            }
-            // 插入到合适位置
-            people[p[1]] = p;
+        List<int[]> res = new ArrayList<>(people.length);
+        for (int[] p : people) {
+            res.add(p[1], p);
         }
-        return people;
+        return res.toArray(new int[res.size()][]);
     }
 }
