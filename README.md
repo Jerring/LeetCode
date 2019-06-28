7ce146d587a1c0d6bdfbebfdb96427dd0c84b025
Jerring <3182730575@qq.com>
2019-06-28 15:09:37 +0800

    Add LeetCode_00149

--- README.md
+++ README.md
@@ -96,6 +96,7 @@
 | 00001 | [Two Sum](https://leetcode.com/problems/two-sum/)            | [两数之和](https://leetcode-cn.com/problems/two-sum/)        |
 | 00036 | [Valid Sudoku](https://leetcode.com/problems/valid-sudoku/)  | [有效的数独](https://leetcode-cn.com/problems/valid-sudoku/) |
 | 00049 | [Group Anagrams](https://leetcode.com/problems/group-anagrams/) | [字母异位词分组](https://leetcode-cn.com/problems/group-anagrams/) |
+| 00149 | [Max Points on a Line](https://leetcode.com/problems/max-points-on-a-line/) | [直线上最多的点数](https://leetcode-cn.com/problems/max-points-on-a-line/) |
 | 00202 | [Happy Number](https://leetcode.com/problems/happy-number/)  | [快乐数](https://leetcode-cn.com/problems/happy-number/)     |
 | 00205 | [Isomorphic Strings](https://leetcode.com/problems/isomorphic-strings/) | [同构字符串](https://leetcode-cn.com/problems/isomorphic-strings/) |
 | 00219 | [Contains Duplicate II](https://leetcode.com/problems/contains-duplicate-ii/) | [存在重复元素 II](https://leetcode-cn.com/problems/contains-duplicate-ii/) |
---
+++ src/com/github/jerring/leetcode/LeetCode_00149.java
@@ -0,0 +1,63 @@
+package com.github.jerring.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode_00149 {
+
+    public int maxPoints(int[][] points) {
+        int n = points.length;
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            // 同一直线上的点的个数（不包含坐标相同的点，坐标相同的点单独计算）
+            Map<Slope, Integer> map = new HashMap<>();
+            int duplicates = 0;
+            for (int j = i + 1; j < n; ++j) {
+                if (points[i][0] == points[j][0] && points[i][1] == points[j][1]) {
+                    ++duplicates;
+                } else {
+                    int x = points[i][0] - points[j][0];
+                    int y = points[i][1] - points[j][1];
+                    int g = gcd(x, y);
+                    Slope slope = new Slope(x / g, y / g);
+                    map.put(slope, map.getOrDefault(slope, 1) + 1);
+                }
+            }
+            // 用坐标相同的点的个数更新最大值
+            res = Math.max(res, duplicates + 1);
+            res = Math.max(res, map.values().stream().max(Integer::compareTo).orElse(0) + duplicates);
+        }
+        return res;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    class Slope {
+        int x, y;
+
+        public Slope(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Slope slope = (Slope) o;
+
+            if (x != slope.x) return false;
+            return y == slope.y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
+    }
+}
