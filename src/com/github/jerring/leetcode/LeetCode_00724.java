0414b36f265967c12bd0747f87ac28a6ea336278
Jerring <3182730575@qq.com>
2019-06-29 11:39:08 +0800

    Add LeetCode_00724

--- src/com/github/jerring/leetcode/LeetCode_00724.java
+++ src/com/github/jerring/leetcode/LeetCode_00724.java
@@ -5,9 +5,6 @@ import java.util.Arrays;
 public class LeetCode_00724 {
 
     public int pivotIndex(int[] nums) {
-        if (nums == null || nums.length < 3) {
-            return -1;
-        }
         int sum = Arrays.stream(nums).sum();
         int s = 0;
         for (int i = 0; i < nums.length; ++i) {
