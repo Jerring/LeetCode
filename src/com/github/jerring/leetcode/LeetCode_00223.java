e350f6617032635ade7c4c4889af2c5115f98430
Jerring <3182730575@qq.com>
2019-07-02 19:31:23 +0800

    Add LeetCode_00223

---
+++ src/com/github/jerring/leetcode/LeetCode_00223.java
@@ -0,0 +1,11 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00223 {
+
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        long x = (long) Math.min(C, G) - Math.max(A, E);
+        long y = (long) Math.min(D, H) - Math.max(B, F);
+        int intersection = x > 0 && y > 0 ? (int) (x * y) : 0;
+        return (C - A) * (D - B) + (G - E) * (H - F) - intersection;
+    }
+}
