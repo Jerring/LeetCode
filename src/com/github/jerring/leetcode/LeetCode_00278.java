7ee800a87cc23e45d18dbd32841ee3c80a138e61
Jerring <3182730575@qq.com>
2019-07-07 19:46:25 +0800

    Add LeetCode_00278

--- src/com/github/jerring/leetcode/LeetCode_00278.java
+++ src/com/github/jerring/leetcode/LeetCode_00278.java
@@ -3,17 +3,13 @@ package com.github.jerring.leetcode;
 public class LeetCode_00278 {
 
     public int firstBadVersion(int n) {
-        int lo = 1;
-        int hi = n;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (isBadVersion(mid)) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + r >>> 1;
+            if (isBadVersion(mid)) r = mid;
+            else l = mid + 1;
         }
-        return lo;
+        return r;
     }
 
     // 题目已定义，无须实现，此处为了避免编译错误而添加
