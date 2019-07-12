582acced69afa5dc913d6ece620ac4cd89536395
Jerring <3182730575@qq.com>
2019-07-12 18:33:32 +0800

    Add LeetCode_01022

---
+++ src/com/github/jerring/leetcode/LeetCode_01022.java
@@ -0,0 +1,20 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_01022 {
+
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int s) {
+        if (root == null) {
+            return 0;
+        }
+        s = s << 1 | root.val;
+        if (root.left == null && root.right == null) {
+            return s;
+        }
+        return dfs(root.left, s) + dfs(root.right, s);
+    }
+}
+
