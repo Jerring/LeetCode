94b746271bb451469a3ccd27175d12cd36d45a28
Jerring <3182730575@qq.com>
2019-07-12 19:01:13 +0800

    Add LeetCode_00700

---
+++ src/com/github/jerring/leetcode/LeetCode_00700.java
@@ -0,0 +1,14 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00700 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
+    }
+}
