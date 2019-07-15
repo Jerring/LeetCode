aa044dbd3b21ee018aaf1033869655906ea92cc5
Jerring <3182730575@qq.com>
2019-07-15 19:03:47 +0800

    Add LeetCode_00139

--- src/com/github/jerring/leetcode/LeetCode_00139.java
+++ src/com/github/jerring/leetcode/LeetCode_00139.java
@@ -24,19 +24,19 @@ public class LeetCode_00139 {
 
     // DP(完全背包)
     public boolean wordBreak(String s, List<String> wordDict) {
-        // 转换为 HashSet 方便查找
         Set<String> set = new HashSet<>(wordDict);
         int n = s.length();
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
+        boolean[] f = new boolean[n + 1];
+        f[0] = true;
         for (int i = 1; i <= n; ++i) {
-            for (int j = i; j >= 0; --j) {
-                if (dp[j] && set.contains(s.substring(j, i))) {
-                    dp[i] = true;
+            for (int j = i - 1; j >= 0; --j) {
+                if (f[j] && set.contains(s.substring(j, i))) {
+                    f[i] = true;
+                    break;
                 }
             }
         }
-        return dp[n];
+        return f[n];
     }
 
 //    // DFS + 剪枝（设置访问标志）
