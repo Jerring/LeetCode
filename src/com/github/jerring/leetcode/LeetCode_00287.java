a5b3a9cc73d374b16e96030d30e753e792d5f6dc
Jerring <3182730575@qq.com>
2019-07-17 09:46:45 +0800

    Add LeetCode_00287

--- src/com/github/jerring/leetcode/LeetCode_00287.java
+++ src/com/github/jerring/leetcode/LeetCode_00287.java
@@ -16,4 +16,20 @@ public class LeetCode_00287 {
         }
         return fast;
     }
+
+//    public int findDuplicate(int[] nums) {
+//        int l = 1, r = nums.length - 1;
+//        while (l < r) {
+//            int mid = l + r >>> 1;
+//            int cnt = 0;
+//            for (int num : nums) {
+//                if (num >= l && num <= mid) {
+//                    ++cnt;
+//                }
+//            }
+//            if (cnt > mid - l + 1) r = mid;
+//            else l = mid + 1;
+//        }
+//        return r;
+//    }
 }
