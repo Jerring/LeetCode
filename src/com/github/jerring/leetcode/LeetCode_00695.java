849e6d66f11e76c2fe183c5ad80985b0653f4e0a
Jerring <3182730575@qq.com>
2019-07-26 09:37:36 +0800

    Add LeetCode_00695

--- src/com/github/jerring/leetcode/LeetCode_00695.java
+++ src/com/github/jerring/leetcode/LeetCode_00695.java
@@ -15,16 +15,10 @@ public class LeetCode_00695 {
     }
 
     private int dfs(int[][] grid, int i, int j) {
-        int sum = 1;
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
         grid[i][j] = 0;
-        if (i - 1 >= 0 && grid[i - 1][j] == 1)
-            sum += dfs(grid, i - 1, j);
-        if (i + 1 < grid.length && grid[i + 1][j] == 1)
-            sum += dfs(grid, i + 1, j);
-        if (j - 1 >= 0 && grid[i][j - 1] == 1)
-            sum += dfs(grid, i, j - 1);
-        if (j + 1 < grid[0].length && grid[i][j + 1] == 1)
-            sum += dfs(grid, i, j + 1);
-        return sum;
+        return 1 + dfs(grid, i - 1, j) + dfs(grid, i, j + 1) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1);
     }
 }
