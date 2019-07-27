2844eb9db148aa8ffedc18f34a35c06695b3afae
Jerring <3182730575@qq.com>
2019-07-27 21:12:10 +0800

    Add LeetCode_00856

---
+++ src/com/github/jerring/leetcode/LeetCode_00856.java
@@ -0,0 +1,19 @@
+package com.github.jerring.leetcode;
+
+public class LeetCode_00856 {
+
+    public int scoreOfParentheses(String S) {
+        int res = 0;
+        for (int i = 0, d = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == '(') {
+                ++d;
+            } else {
+                --d;
+                if (S.charAt(i - 1) == '(') {
+                    res += 1 << d;
+                }
+            }
+        }
+        return res;
+    }
+}
