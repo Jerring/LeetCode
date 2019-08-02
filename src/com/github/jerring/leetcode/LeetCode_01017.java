682e548bc4b970020ef3157ce38e2fde02694fce
Jerring <3182730575@qq.com>
2019-08-02 21:09:10 +0800

    Add LeetCode_01017

--- src/com/github/jerring/leetcode/LeetCode_01017.java
+++ src/com/github/jerring/leetcode/LeetCode_01017.java
@@ -17,15 +17,12 @@ public class LeetCode_01017 {
 //    }
 
     public String baseNeg2(int N) {
-        if (N == 0) {
-            return "0";
-        }
         StringBuilder sb = new StringBuilder();
         while (N != 0) {
             sb.append(N & 1);
             N = -(N >> 1);
         }
-        return sb.reverse().toString();
+        return sb.length() > 0 ? sb.reverse().toString() : "0";
     }
 }
 
