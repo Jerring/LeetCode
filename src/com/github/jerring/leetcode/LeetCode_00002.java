9162642be4b1a9bb1eaa69bce5cfb1a80e6d07c9
Jerring <3182730575@qq.com>
2019-08-03 21:38:07 +0800

    Add LeetCode_00002

--- src/com/github/jerring/leetcode/LeetCode_00002.java
+++ src/com/github/jerring/leetcode/LeetCode_00002.java
@@ -3,21 +3,16 @@ package com.github.jerring.leetcode;
 public class LeetCode_00002 {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1);
+        ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
         int carry = 0;
-        while (l1 != null || l2 != null) {
-            int x = l1 != null ? l1.val : 0;
-            int y = l2 != null ? l2.val : 0;
-            int sum = x + y + carry;
-            carry = sum / 10;
-            cur.next = new ListNode(sum % 10);
+        while (l1 != null || l2 != null || carry != 0) {
+            carry += (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0);
+            cur.next = new ListNode(carry % 10);
             cur = cur.next;
+            carry /= 10;
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
-        }
-        if (carry > 0) {
-            cur.next = new ListNode(carry);
         }
         return dummy.next;
     }
