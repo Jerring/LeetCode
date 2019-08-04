3287f4b20448e79dda932c0b17e15cc768a8d0e0
Jerring <3182730575@qq.com>
2019-08-04 16:15:28 +0800

    Add LeetCode_00257

--- src/com/github/jerring/leetcode/LeetCode_00257.java
+++ src/com/github/jerring/leetcode/LeetCode_00257.java
@@ -5,24 +5,45 @@ import java.util.List;
 
 public class LeetCode_00257 {
 
+//    public List<String> binaryTreePaths(TreeNode root) {
+//        List<String> res = new ArrayList<>();
+//        if (root != null) {
+//            dfs(root, String.valueOf(root.val), res);
+//        }
+//        return res;
+//    }
+//
+//    private void dfs(TreeNode root, String path, List<String> res) {
+//        if (root.left == null && root.right == null) {
+//            res.add(path);
+//            return;
+//        }
+//        if (root.left != null) {
+//            dfs(root.left, path + "->" + root.left.val, res);
+//        }
+//        if (root.right != null) {
+//            dfs(root.right, path + "->" + root.right.val, res);
+//        }
+//    }
+
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
-        if (root != null) {
-            dfs(root, String.valueOf(root.val), res);
+        if (root == null) {
+            return res;
+        }
+        String cur = String.valueOf(root.val);
+        if (root.left == null && root.right == null) {
+            res.add(cur);
+            return res;
+        }
+        List<String> list = binaryTreePaths(root.left);
+        for (String s : list) {
+            res.add(cur + "->" + s);
+        }
+        list = binaryTreePaths(root.right);
+        for (String s : list) {
+            res.add(cur + "->" + s);
         }
         return res;
-    }
-
-    private void dfs(TreeNode root, String path, List<String> res) {
-        if (root.left == null && root.right == null) {
-            res.add(path);
-            return;
-        }
-        if (root.left != null) {
-            dfs(root.left, path + "->" + root.left.val, res);
-        }
-        if (root.right != null) {
-            dfs(root.right, path + "->" + root.right.val, res);
-        }
     }
 }
