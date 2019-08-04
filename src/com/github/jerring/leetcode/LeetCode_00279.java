a7bba3a82aff15b60cdd3b36ac111dfb652d542c
Jerring <3182730575@qq.com>
2019-08-04 10:19:30 +0800

    Add LeetCode_00279

--- src/com/github/jerring/leetcode/LeetCode_00279.java
+++ src/com/github/jerring/leetcode/LeetCode_00279.java
@@ -3,53 +3,26 @@ package com.github.jerring.leetcode;
 public class LeetCode_00279 {
 
 //    public int numSquares(int n) {
-//        List<Integer> squares = generateSquares(n);
-//        Queue<Integer> queue = new LinkedList<>();
-//        // 访问标志，用于剪枝
+//        Queue<int[]> queue = new ArrayDeque<>();
 //        boolean[] vis = new boolean[n + 1];
+//        queue.offer(new int[]{n, 0});
 //        vis[n] = true;
-//        queue.offer(n);
-//        int level = 0;
 //        while (!queue.isEmpty()) {
-//            ++level;
-//            int size = queue.size();
-//            while (size-- != 0) {
-//                int cur = queue.poll();
-//                for (int s : squares) {
-//                    int next = cur - s;
-//                    if (next < 0) {
-//                        break;
-//                    }
-//                    if (next == 0) {
-//                        return level;
-//                    }
-//                    // 剪枝（之前出现过现在出现不可能达到最小）
-//                    if (vis[next]) {
-//                        continue;
-//                    }
-//                    vis[next] = true;
-//                    queue.offer(next);
+//            int[] cur = queue.poll();
+//            for (int i = 1; ; ++i) {
+//                int x = cur[0] - i * i;
+//                if (x < 0) {
+//                    break;
+//                } else if (x == 0) {
+//                    return cur[1] + 1;
+//                }
+//                if (!vis[x]) {
+//                    queue.offer(new int[]{x, cur[1] + 1});
+//                    vis[x] = true;
 //                }
 //            }
 //        }
-//        return n;
-//    }
-//
-//    /**
-//     * 生成 n 以内的完全平方数
-//     * @param n 输入
-//     * @return  平方数的集合
-//     */
-//    private List<Integer> generateSquares(int n) {
-//        List<Integer> squares = new ArrayList<>();
-//        int square = 1;
-//        int diff = 3;
-//        while (square <= n) {
-//            squares.add(square);
-//            square += diff;
-//            diff += 2;
-//        }
-//        return squares;
+//        throw new IllegalArgumentException();
 //    }
 
     public int numSquares(int n) {
